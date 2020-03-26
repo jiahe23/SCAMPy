a129ae75fbcc9e8c4b5aaf9f502809095aeb3337
Jia He <jiahe.gatech@gmail.com>
2020-03-25 20:15:40 -0700

    .

--- generate_paramlist.py
+++ generate_paramlist.py
@@ -54,7 +54,7 @@ def main():
     paramlist_defaults['turbulence']['EDMF_PrognosticTKE']['sorting_power'] = 2.0
     paramlist_defaults['turbulence']['EDMF_PrognosticTKE']['aspect_ratio'] = 0.2
 
-    paramlist_defaults['turbulence']['EDMF_PrognosticTKE']['Smagorinsky_Lilly_coeff'] = 0.75
+    paramlist_defaults['turbulence']['EDMF_PrognosticTKE']['Smagorinsky_Lilly_coeff'] = 0.6
     # This constant_plume_spacing corresponds to plume_spacing/alpha_d in the Tan et al paper,
     #with values plume_spacing=500.0, alpha_d = 0.375
     paramlist_defaults['turbulence']['EDMF_PrognosticTKE']['constant_plume_spacing'] = 1333.0
